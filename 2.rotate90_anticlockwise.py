0b1de3d8a52838a5861ea7ef13e2e5f3c2ff2567
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-01-03 22:05:44 +0530

    Added rotate90_anticlockwise.py

---
+++ 2.rotate90_anticlockwise.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+def rotate(matrix): 
+    #code here
+    n = len(matrix)
+    
+    for i in range(n):
+        for j in range(i,n):
+            matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+            
+    for i in range(0,n//2):
+        for j in range(0,n):
+            matrix[i][j], matrix[n-1-i][j] = matrix[n-1-i][j], matrix[i][j] 
+            
+    #rint(matrix)
+        
+    return matrix
+    
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__':
+	t = int(input())
+	for _ in range(t):
+		N=int(input())
+		arr=[int(x) for x in input().split()]
+		matrix=[]
+
+		for i in range(0,N*N,N):
+			matrix.append(arr[i:i+N])
+
+		rotate(matrix)
+		for i in range(N): 
+			for j in range(N): 
+				print(matrix[i][j], end =' ') 
+			print() 
+        
+
+# } Driver Code Ends
