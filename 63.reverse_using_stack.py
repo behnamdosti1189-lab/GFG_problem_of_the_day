5eab6719db7297c2dbc375b08b769bccebae6fa4
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-03-08 10:56:15 +0530

    added reverse_using_stack

---
+++ 63.reverse_using_stack.py
@@ -0,0 +1,31 @@
+'''
+Reverse a string using Stack 
+
+You are given a string S, the task is to reverse the string using stack.
+
+https://practice.geeksforgeeks.org/problems/reverse-a-string-using-stack/1
+'''
+
+#Driver Code Starts
+
+ # } Driver Code Ends
+def reverse(S):
+    stack = []
+    for i in S:
+        stack.append(i)
+    n = len(stack)
+    res=''
+    while(n>0):
+        res+=stack.pop()
+        n-=1
+    return res
+    #Add code here
+
+#{ 
+#Driver Code Starts.
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        str1=input()
+        print(reverse(str1))
+#} Driver Code Ends
