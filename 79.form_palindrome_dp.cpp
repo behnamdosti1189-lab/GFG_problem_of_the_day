bfaf41f9ec0b4dbf474ef2478f91fceb06da9ddd
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-03-24 22:48:02 +0530

    added form_palindrome_dp

---
+++ 79.form_palindrome_dp.cpp
@@ -0,0 +1,68 @@
+// Form a palindrome 
+
+// Given a string, find the minimum number of characters to be inserted to convert it to palindrome.
+// For Example:
+// ab: Number of insertions required is 1. bab or aba
+// aa: Number of insertions required is 0. aa
+// abcd: Number of insertions required is 3. dcbabcd
+
+// https://practice.geeksforgeeks.org/problems/form-a-palindrome2544/1#
+
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int lcs(string s1,string s2){
+        int m=s1.length();
+        int n=s2.length();
+        int dp[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    int findMinInsertions(string S){
+        string s2=S;
+        reverse(s2.begin(),s2.end());
+        //this question is about finding longest pallindrome subsequence
+        int n=S.size();
+        return (n-lcs(S,s2));
+    }
+};
+
+// { Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.findMinInsertions(S)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
