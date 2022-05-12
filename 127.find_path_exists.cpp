61ee4bf9fff5cdb7a142278284f012c3b57616a8
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-05-12 12:44:12 +0530

    added find_path_exists

---
+++ 127.find_path_exists.cpp
@@ -0,0 +1,54 @@
+// Find whether path exist 
+// Given a grid of size n*n filled with 0, 1, 2, 3. Check whether there is a path possible from the source to destination. You can traverse up, down, right and left.
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find whether a path exists from the source to destination.
+    bool dfs(int x, int y, vector<vector<int>>& grid, int &n, int &m){
+        if(x < 0 || x == n || y < 0 || y == m || grid[x][y] == 0) return false;
+        if(grid[x][y] == 2) return true;
+        grid[x][y] = 0;
+        return dfs(x - 1, y, grid, n, m) || dfs(x + 1, y, grid, n, m) || dfs(x, y + 1, grid, n, m) || dfs(x, y - 1, grid, n, m);
+    }
+    bool is_Possible(vector<vector<int>>& grid) 
+    {
+        //code here
+        int n = grid.size(), m = grid[0].size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1){
+                    if(dfs(i, j, grid, n, m)) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>grid(n, vector<int>(n, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		bool ans = obj.is_Possible(grid);
+		cout << ((ans) ? "1\n" : "0\n");
+	}
+	return 0;
+}  // } Driver Code Ends
