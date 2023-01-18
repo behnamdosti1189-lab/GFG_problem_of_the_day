74e703b399f4f0c4f8ac33a712dd260d5b3afb0b
Dharmesh Poladiya <45046311+Dharm3438@users.noreply.github.com>
2023-01-18 09:44:11 +0530

    Create 156.first_node_of_loop_ll.py

---
+++ 156.first_node_of_loop_ll.py
@@ -0,0 +1,23 @@
+class Solution:
+    #Function to find first node if the linked list has a loop.
+    def findFirstNode(self, head):
+        #code here
+        
+        slow=head
+        fast=head
+        
+        while(fast and fast.next):
+            slow=slow.next
+            fast=fast.next.next
+            if(slow==fast):
+                break
+        
+        if(slow!=fast):
+            return -1
+        
+        slow = head
+        while(slow!=fast):
+            slow=slow.next
+            fast = fast.next
+        
+        return slow.data
