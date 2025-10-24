3852704706b186f0bf6dc4d97a9d352b5c8502d5
behnamdosti1189-lab <behnamdosti1189@gmail.com>
2025-10-25 01:13:34 +0330

    Update 57.exactly_one_swap.py

--- 57.exactly_one_swap.py
+++ 57.exactly_one_swap.py
@@ -4,7 +4,7 @@ Exactly one swap
 Given a string S containing lowercase english alphabet characters. The task is to calculate the number of distinct strings that can be obtained after performing exactly one swap.
 In one swap,Geek can pick two distinct index i and j (i.e 1 < i < j < |S| ) of the string, then swap the characters at the position i and j.
 
-https://practice.geeksforgeeks.org/problems/2ac2f925b836b0625d848a0539ffd3d2d2995f92/1#
+(URL)https://practice.geeksforgeeks.org/problems/2ac2f925b836b0625d848a0539ffd3d2d2995f92/1#
 '''
 
 #User function Template for python3
@@ -20,9 +20,9 @@ class Solution:
             else:
                 charSet[i] = 0
         ans = (n*(n-1)) // 2
-        if(count >0):
-            ans = ans - count + 1
-        return ans
+#        if(count >0):
+#            ans = ans - count + 1
+#        return ans
 
 #{ 
 #  Driver Code Starts
@@ -30,11 +30,11 @@ class Solution:
 
 
 
-if __name__ == '__main__': 
-    t = int(input())
-    for _ in range(t):
-        S = input()
-        ob = Solution()
-        ans = ob.countStrings(S)
-        print(ans)
-# } Driver Code Ends
+#if __name__ == '__main__': 
+#    t = int(input())
+#    for _ in range(t):
+#        S = input()
+#        ob = Solution()
+#        ans = ob.countStrings(S)
+#        print(ans)
+# 
